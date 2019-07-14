3e21d980bf96b5b8102ec0d34c2de6c76f0a6bbd
iromaniuk@advisor360.com <docker@PF1L54YP.advisor360corp.com>
2019-07-14 15:50:50 -0400

    Housekeeping

--- README.md
+++ README.md
@@ -10,3 +10,41 @@
 - WsControlUser : "" #Workspot Control Administrator user email address
 - WsControlPass : "" #Workspot Control Administrator user password
 ```
+
+### Install and Run
+
+```
+$python3.7 -m pip install -r requirements.txt
+$python3.7 wpan.py
+```
+
+
+####
+
+```
+admin@PA-200> show user ip-user-mapping all
+(container-tag: user container-tag: ip-user-mapping container-tag: all pop-tag: pop-tag: pop-tag:)
+((eol-matched: . #t) (context-inserted-at-end-p: . #f))
+
+
+<request cmd="op" cookie="6777592993635321" uid="500"><operations><show><user><ip-user-mapping><all/></ip-user-mapping></user></show></operations></request>
+
+
+2019-07-14 23:40:52
+<response status="success"><result>IP                                            Vsys   From    User                             IdleTimeout(s) MaxTimeout(s)
+--------------------------------------------- ------ ------- -------------------------------- -------------- -------------
+1.1.1.1                                       vsys1  XMLAPI  user1                            1802           1802
+4.4.4.4                                       vsys1  XMLAPI  user4                            215994         215994
+2.2.2.2                                       vsys1  XMLAPI  user2                            2702           2702
+3.3.3.3                                       vsys1  XMLAPI  user3                            Never          Never
+Total: 4 users
+</result></response>
+```
+
+IP                                            Vsys   From    User                             IdleTimeout(s) MaxTimeout(s)
+--------------------------------------------- ------ ------- -------------------------------- -------------- -------------
+1.1.1.1                                       vsys1  XMLAPI  user1                            1802           1802
+4.4.4.4                                       vsys1  XMLAPI  user4                            215994         215994
+2.2.2.2                                       vsys1  XMLAPI  user2                            2702           2702
+3.3.3.3                                       vsys1  XMLAPI  user3                            Never          Never
+Total: 4 users
--- wpan.py
+++ wpan.py
@@ -4,7 +4,8 @@
 # Worskpot PAN User-ID integration #
 ####################################
 
-import sys, base64, requests, json, asyncio, logging
+import sys, base64, json, logging 
+import requests, asyncio, pan.xapi
 from aiohttp import ClientSession
 
 # Variable initialization #
@@ -31,23 +32,17 @@ async def workspot(url,headers):
             response = await response.read()            
             return response
 
-def get_token():
+def get_token(ApiClientPair, Username, Password):
     """ Get authorization token from Workspot API """
-    with open("config.yaml", 'r') as stream:
-        try:
-            config = yaml.safe_load(stream)
-            logger.info(f"--->{config}")
-        except yaml.YAMLError as exc:
-            logger.err(f"--->{exc}")
-    ApiClientPair = config[0]["ApiClientId"] + ':' + config[1]["ApiClientSecret"]
+    
     EncodedApiCreds = base64.b64encode(ApiClientPair.encode('ascii'))
     HeaderAuthValue = b"Basic " + EncodedApiCreds
     Headers = { 'Authorization': '{}'.format(HeaderAuthValue.decode('ascii'))}
     logger.info("-->>Authorization [%s]", Headers["Authorization"])
     url = "https://api.workspot.com/oauth/token" #?grant_type=password
     payload = { # "Content-Type": "x-www-form-urlencoded", grant_type": 'password', "grant_type": 'client_credentials'
-        'username': config[2]["WsControlUser"],
-        'password': config[3]["WsControlPass"],
+        'username': Username,
+        'password': Password,
         'Content-Type': 'x-www-form-urlencoded',
         'grant_type': 'password'
         }   
@@ -74,6 +69,54 @@ def get_desktops(j):
             logger.debug(('--->{0:<20} {1:<3} {2:<20} {3:<30}'.format(i["name"],i["email"],i["ipAddress"],i["status"])))
             userid.append(("Workspot_"+i["poolName"]+ "\\" + i["email"], i["ipAddress"]))
     return userid    
+
+def panuserid(panapi, panhost, xml):
+    try:
+        xapi = pan.xapi.PanXapi(api_key=panapi, hostname=panhost)
+    except pan.xapi.PanXapiError as msg:
+        print('pan.xapi.PanXapi:', msg, file=sys.stderr)
+        sys.exit(1)
+
+    xpath = "/api/"
+
+    try:
+        xapi.user_id(cmd=xml, vsys=None)
+    except pan.xapi.PanXapiError as msg:
+        print('user-id:', msg, file=sys.stderr)
+        sys.exit(1)
+
+    print('uid applied')
+
+def set_user(user, address, timeout):
+    uid_xml = """
+    '''<uid-message>
+    <type>update</type>
+    <payload>
+        <login>
+        <entry name="{}" ip="{}" timeout="{}"/>
+        </login>
+    </payload>
+    </uid-message>'''
+    """.format(user, address, timeout)
+    return uid_xml    
+    
+def set_group(user, group):
+    uid_xml = """
+    '''<uid-message>
+  <type>update</type>
+  <payload>
+    <groups>
+      <entry name="group1">
+        <members>
+          <entry name="user1"/>
+        </members>
+      </entry>
+    </groups>
+  </payload>
+</uid-message>'''
+    """.format(user, address, timeout)
+    return uid_xml    
+    
     
 async def main(url, Headers): 
     #res = await asyncio.gather(workspot(url, Headers))  
@@ -85,7 +128,17 @@ if __name__ == "__main__":
     from itertools import chain
 
     assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
-    Headers = get_token()
+    with open("config.yaml", 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+            logger.info(f"--->{config}")
+        except yaml.YAMLError as exc:
+            logger.err(f"--->{exc}")
+    ApiClientPair = config[0]["ApiClientId"] + ':' + config[1]["ApiClientSecret"]
+    Username = config[2]["WsControlUser"],
+    Password = config[3]["WsControlPass"],
+
+    Headers = get_token(ApiClientPair, Username, Password)
     urls = [URL]
     start = time.perf_counter()
     r = asyncio.run(main(urls, Headers))
@@ -101,6 +154,10 @@ if __name__ == "__main__":
     logger.info(f"--->{userid}")
     end = time.perf_counter() - start
     logger.info(f"--->finished in {end:0.2f} seconds.")
+
+    xml = set_user("user5", "5.5.5.5", 3600)
+    logger.debug(f"--->{xml}")
+    panuserid(config[4]["PanApi"],"192.168.3.1", xml)
     
    
                 
